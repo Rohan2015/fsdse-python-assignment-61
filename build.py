ec5e8126f67d36c50baac4d45237af193083ebbf
Rohan2015 <rohandamodar@outlook.com>
2017-06-19 10:32:54 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,16 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    even_count = 0
+    odd_count = 0
+    for i in list_of_nums:
+        if i%2 == 0:
+            even_count = even_count + 1
+        else:
+            odd_count = odd_count + 1
+    #print even_count, odd_count
+    keys = ['ODD','EVEN']
+    values = [odd_count,even_count]
+    dic = dict(zip(keys,values))
+    #print d
+    return dic
+
+solution([1, 2, 3, 5, 8, 9])
